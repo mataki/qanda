436bbc7aa27ce7897a22fdc92261d77344e02631
AkihiroMatsumura <matsumura.aki@gmail.com>
2008-12-01 18:46:24 +0900

    質問が見える人かどうか確認するメソッドを追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -16,6 +16,21 @@ class Question < ActiveRecord::Base
     viewer_regexs.build conversion_regex_str_to_rebex_hash(regexs_str)
   end
 
+  def viewer?(identity_url)
+    viewer_regexs.any? do |regex|
+      Regexp.new(regex.regex).match(identity_url)
+    end
+  end
+
+  def owner?(identity_url)
+    owner_regexs.any? do |regex|
+      Regexp.new(regex.regex).match(identity_url)
+    end
+  end
+
+  def accsessible?(identity_url)
+    viewer? or owner?
+  end
 private
   def conversion_regex_str_to_rebex_hash(regexs_str)
     regexs = regexs_str ? regexs_str.split(',') : []
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -23,3 +23,26 @@ describe Question, "#owner_regexs=" do
     @question.owner_regexs.length.should == 2
   end
 end
+
+describe Question, "#viewer?" do
+  describe "viewの正規表現に引っかかる場合" do
+    before do
+      @regexs = (1..2).map{|i| stub_model(OpenidRegex, :regex => "http://hoge#{i}.com/.*")}
+      @identity_url = "http://fuga.com/id"
+      @question = stub_model(Question, :viewer_regexs => @regexs)
+    end
+    it "falseを返す" do
+      @question.viewer?(@identity_url).should be_false
+    end
+  end
+  describe "viewの正規表現に引っかからない場合" do
+    before do
+      @regexs = (1..2).map{|i| stub_model(OpenidRegex, :regex => "http://hoge#{i}.com/.*")}
+      @identity_url = "http://hoge1.com/111111"
+      @question = stub_model(Question, :viewer_regexs => @regexs)
+    end
+    it "trueを返す" do
+      @question.viewer?(@identity_url).should be_true
+    end
+  end
+end
