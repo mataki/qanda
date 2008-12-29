0a27102946db5938b4eb7118a661ef7e4137a713
mat_aki <matsumura@openskip.org>
2008-12-30 01:34:29 +0900

    use AR#serialize when save content of answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,11 +3,13 @@ class Answer < ActiveRecord::Base
 
   validates_presence_of :identity_url
 
+  serialize :content
+
   def set_content(params)
     orign_params = params.dup
     orign_params.delete_if do |key, value|
       ["commit","authenticity_token", "action", "controller", "question_id"].include?(key)
     end
-    self.content = orign_params.to_json
+    self.content = orign_params
   end
 end
