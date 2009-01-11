79cdd6dfc24c78d9dcd6172fc59cf5c09a0b7c60
mat_aki <matsumura.aki@gmail.com>
2009-01-11 10:34:29 +0900

    to work on heroku

--- vendor/plugins/open_id_authentication/init.rb
+++ vendor/plugins/open_id_authentication/init.rb
@@ -1,5 +1,5 @@
 if config.respond_to?(:gems)
-  config.gem 'ruby-openid', :lib => 'openid', :version => '>=2.0.4'
+  config.gem 'ruby-openid', :lib => 'openid', :version => '>=2.1.2'
 else
   begin
     require 'openid'
