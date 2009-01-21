e071abd3566dd337281a5f9446796d1dcd392625
mat_aki <matsumura.aki@gmail.com>
2009-01-21 17:58:12 +0900

    jquery で複数回 corner されている要素があったのを修正

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,5 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function(){
-    $('.corner').corner();
-    $('h1').corner();
-    $('h3').corner();
+    $('h1, h3, .corner').corner();
 });
